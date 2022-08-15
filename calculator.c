9594884e6379c832509b2adee1ac720fc9bd6098
AKoth-Otieno <elsyakoth@gmail.com>
2022-08-15 01:44:02 -0700

    Allow user to enter input into the calculator. Read user input.

--- calculator.c
+++ calculator.c
@@ -2,6 +2,25 @@
 
 int main()
 {
+	char operator;
+	double num_1, num_2;
+
+	/*Ask user for input.*/
+	printf("Select Operator(+, -, *, or /)");
+
+	/*Assign user input to the variable operator.'*/
+	scanf("%c", &operator);
+
+	/*Ask user to enter any two numbers on which to perform operation.*/
+	printf("Enter two numbers, one after another: ");
+
+	/*Read user input and assign it to the two double variables.*/
+	scanf("%lf %lf",&num_1,&num_2);
+
+	/*The logic for the calculator*/
+
+
+
 
 	return (0);
 }
