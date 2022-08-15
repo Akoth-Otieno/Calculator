109e632fd186337e781e4e1dfeefbb04b19c912d
AKoth-Otieno <elsyakoth@gmail.com>
2022-08-15 02:08:01 -0700

    Add the logic of the calculator.

--- calculator.c
+++ calculator.c
@@ -17,7 +17,39 @@ int main()
 	/*Read user input and assign it to the two double variables.*/
 	scanf("%lf %lf",&num_1,&num_2);
 
-	/*The logic for the calculator*/
+	/*The logic for the calculator using switch statement*/
+	switch
+	{
+		case '+':
+			printf("%lf + %lf = lf", num_1,num_2,(num_1+num_2));
+			break;
+
+		case '-':
+                        printf("%lf - %lf = lf", num_1,num_2,(num_1-num_2));
+                        break;
+
+		case '*':
+                        printf("%lf * %lf = lf", num_1,num_2,(num_1*num_2));
+                        break;
+
+		case '/':
+			if (num_2 =! 0.0)
+				printf("%lf / %lf = lf", num_1,num_2,(num_1/num_2));
+			else
+				printf("Divide by Zero");
+			break;
+
+		default:
+			printf("Invalid Operator");
+
+			break;
+
+	return (0);
+	
+
+	}
+
+
 
 
 
